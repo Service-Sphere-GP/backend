9ed6697fb2c7fe6343d07cf3ae1b6b757280a292
3ein39 <hussein.hany.cs@gmail.com>
2025-01-27 13:54:14 +0200

    Add CreateServiceDto and update ServiceDto and Service schema for improved service management

--- src/services/dto/create-service.dto.ts
+++ src/services/dto/create-service.dto.ts
@@ -1,35 +1,34 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsNumber, IsArray, IsOptional } from 'class-validator';
 import { Types } from 'mongoose';
 
-export class ServiceDto {
+export class CreateServiceDto {
   @ApiProperty({ example: 'Plumbing' })
+  @IsString()
   service_name: string;
 
   @ApiProperty({ type: Object, example: { availability: '24/7' } })
   service_attributes: Record<string, string>;
 
   @ApiProperty({ example: 100 })
+  @IsNumber()
   base_price: number;
 
   @ApiProperty({ example: 'active' })
+  @IsString()
   status: string;
 
   @ApiProperty({ example: 'Plumbing services' })
+  @IsOptional()
+  @IsString()
   description?: string;
 
   @ApiProperty({ example: 'Home Services' })
+  @IsOptional()
+  @IsString()
   category?: string;
 
-  @ApiProperty({ example: '2021-09-01T00:00:00.000Z' })
-  creation_time: Date;
-
-  @ApiProperty({
-    type: [String],
-    example: ['https://example.com/image1.jpg', 'https://example.com/image2.jpg'],
-    description: 'Array of image URLs associated with the service',
-  })
-  images: string[];
 
   @ApiProperty({ example: "ObjectId('6794d963db9ce9d9caa11260')" })
-  service_provider_id?: Types.ObjectId;
+  service_provider_id: Types.ObjectId;
 }
--- src/users/schemas/service-provider.schema.ts
+++ src/users/schemas/service-provider.schema.ts
@@ -1,6 +1,10 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { User } from './user.schema';
 import { Service, ServiceSchema } from '../../services/schemas/service.schema';
+import { Document } from 'mongoose';
+
+export type ServiceProviderDocument = ServiceProvider & Document;
+
 
 @Schema()
 export class ServiceProvider extends User {
