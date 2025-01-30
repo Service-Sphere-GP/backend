68b471e7385732f8e704f933e15373951e8ce370
hussein-saad <hussein.saad2021@gmail.com>
2025-01-30 17:24:43 +0200

    Update JWT expiration time to use configurable value from environment

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -18,7 +18,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         secret: configService.get('JWT_SECRET'),
-        signOptions: { expiresIn: '1h' },
+        signOptions: { expiresIn: configService.get('JWT_EXPIRATION_TIME') },
       }),
     }),
   ],
