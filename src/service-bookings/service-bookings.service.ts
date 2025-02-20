4c126eeff0720a94bc7a47a8356d66daeabea19d
Hussein Saad <hussein.saad2021@gmail.com>
2025-02-21 00:36:28 +0200

    Add ticket creation upon booking in BookingsService

--- src/service-bookings/service-bookings.service.ts
+++ src/service-bookings/service-bookings.service.ts
@@ -1,14 +1,22 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  BadRequestException,
+  Inject,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { ServiceBookings } from './schemas/service-booking.schema';
 import { ServicesService } from '../services/services.service';
+import { Ticket } from '../tickets/schemas/ticket.schema'; 
+import { Service } from '../services/schemas/service.schema'; 
 
 @Injectable()
 export class BookingsService {
   constructor(
     @InjectModel(ServiceBookings.name)
     private bookingModel: Model<ServiceBookings>,
+    @InjectModel(Ticket.name) private ticketModel: Model<Ticket>,
     private readonly servicesService: ServicesService,
   ) {}
 
@@ -25,7 +33,15 @@ export class BookingsService {
       customer_id: customerId,
       service_id: serviceId,
     });
+    const savedBooking = await booking.save();
 
-    return booking.save();
+    const ticketData = {
+      booking_id: savedBooking._id,
+      status: 'open',
+    };
+
+    await this.ticketModel.create(ticketData);
+
+    return savedBooking;
   }
 }
