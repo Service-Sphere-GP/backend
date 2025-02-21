522b0c2d742f93dd82777b0b1a35725856ed1975
Hussein Saad <hussein.saad2021@gmail.com>
2025-02-21 02:20:48 +0200

    Add TicketsController for managing ticket retrieval and status updates

--- src/tickets/tickets.module.ts
+++ src/tickets/tickets.module.ts
@@ -3,14 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { TicketsService } from './tickets.service';
 import { TicketsController } from './tickets.controller';
 import { TicketSchema } from './schemas/ticket.schema';
-import { ServiceProviderSchema } from '../users/schemas/service-provider.schema';
-import { ServiceBookingsSchema } from './../service-bookings/schemas/service-booking.schema';
+import { UserModule } from 'src/users/user.module';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([
-      { name: 'Ticket', schema: TicketSchema },
-    ]),
+    MongooseModule.forFeature([{ name: 'Ticket', schema: TicketSchema }]),
+    UserModule,
   ],
   providers: [TicketsService],
   controllers: [TicketsController],
