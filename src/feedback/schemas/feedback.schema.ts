4b6dee2be51216c1fc1c769bbf77ee8bffcacc4a
Hussein Hany <hussein.hany.cs@gmail.com>
2025-03-16 14:22:43 +0200

    Feat: Enhance feedback DTO and controller with new properties and Swagger documentation

--- src/feedback/schemas/feedback.schema.ts
+++ src/feedback/schemas/feedback.schema.ts
@@ -10,13 +10,16 @@ export class Feedback extends Document {
   message: string;
 
   @Prop({ type: Types.ObjectId, ref: 'Service', default: null })
-  given_to_service?: Types.ObjectId;
-
-  @Prop({ type: Types.ObjectId, ref: 'ServiceProvider', default: null })
-  given_to_provider?: Types.ObjectId;
+  about_service?: Types.ObjectId;
 
   @Prop({ type: Types.ObjectId, ref: 'User', default: null })
-  given_to_customer?: Types.ObjectId;
+  about_customer?: Types.ObjectId;
+
+  @Prop({ type: Types.ObjectId, ref: 'ServiceProvider', default: null })
+  from_provider?: Types.ObjectId;
+
+  @Prop({ type: Types.ObjectId, ref: 'User', default: null })
+  from_customer?: Types.ObjectId;
 }
 
 export const FeedbackSchema = SchemaFactory.createForClass(Feedback);
