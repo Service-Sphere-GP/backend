c2b49f74fc487ec8089a96c93b475ffb49be6573
Hussein Saad <hussein.saad2021@gmail.com>
2025-04-09 14:01:37 +0200

    Refactor: Remove Ticket module and related references from service bookings

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,10 +9,8 @@ import { UserModule } from './users/user.module';
 import { ServicesModule } from './services/services.module';
 import { CloudinaryModule } from 'nestjs-cloudinary';
 import { AuthModule } from './auth/auth.module';
-import { ServiceBookingsModule } from './service-bookings/service-bookings.module';
-import { Feedback } from './feedback/schemas/feedback.schema';
+import { ServiceBookingsModule } from './service-bookings/service-bookings.module'
 import { FeedbackModule } from './feedback/feedback.module';
-import { TicketsModule } from './tickets/tickets.module';
 
 @Module({
   imports: [
@@ -41,7 +39,6 @@ import { TicketsModule } from './tickets/tickets.module';
     AuthModule,
     ServiceBookingsModule,
     FeedbackModule,
-    TicketsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/service-bookings/service-bookings.service.ts
+++ src/service-bookings/service-bookings.service.ts
@@ -8,14 +8,12 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model, Types } from 'mongoose';
 import { ServiceBookings } from './schemas/service-booking.schema';
 import { ServicesService } from '../services/services.service';
-import { Ticket } from '../tickets/schemas/ticket.schema';
 import { UsersService } from './../users/users.service';
 @Injectable()
 export class BookingsService {
   constructor(
     @InjectModel(ServiceBookings.name)
     private bookingModel: Model<ServiceBookings>,
-    @InjectModel(Ticket.name) private ticketModel: Model<Ticket>,
     private readonly servicesService: ServicesService,
     private readonly UsersService: UsersService,
   ) {}
@@ -56,16 +54,6 @@ export class BookingsService {
         });
 
         const savedBooking = await booking.save();
-
-        const ticketData = {
-          _id: ticketId,
-          booking_id: savedBooking._id,
-          status: 'open',
-          assigned_to: provider._id,
-          createdBy: new Types.ObjectId(customerId),  
-        };
-
-        await this.ticketModel.create(ticketData);
 
         return savedBooking;
       } catch (dbError) {
