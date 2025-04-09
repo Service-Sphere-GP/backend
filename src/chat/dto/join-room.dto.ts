8c89f751b5ae21ca6e40048535cc6bc5a88ef7ae
Hussein Saad <hussein.saad2021@gmail.com>
2025-04-10 01:16:56 +0200

    Refactor: Remove unused JoinRoomDto class and related imports

--- src/chat/dto/join-room.dto.ts
+++ src/chat/dto/join-room.dto.ts
@@ -1,9 +1,8 @@
-import { IsMongoId, IsNotEmpty } from 'class-validator';
+import { IsNotEmpty } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class JoinRoomDto {
   @IsNotEmpty()
-  @IsMongoId()
   @ApiProperty({ description: 'The ID of the booking to join the chat for' })
   bookingId: string;
 }
